e21d04c212821504fdbbcae1cc2fb9c42548ecd6
atamayo <atamayoquintero@gmail.com>
2023-03-17 18:51:58 -0500

    fix: upgrade App with new components and global sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import logo from './logo.svg'
+import { Container, Row, Col } from 'react-bootstrap'
+import { TodoCounter } from './components/todoCounter'
+import { TodoSearch } from './components/todoSearch'
+import { TodoList } from './components/todoList'
+import { TodoItem } from './components/todoItem'
+import { TodoCreateButton } from './components/todoCreateButton'
 
 function App() {
   return (
     <React.Fragment>
-      <Container className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Container>
+        <Row>
+          <Col sm={12}>
+            <TodoCounter />
+          </Col>
+          <Col sm={12}>
+            <TodoSearch />
+          </Col>
+          <Col sm={12}>
+            <TodoList>
+              <TodoItem />
+            </TodoList>
+          </Col>
+          <Col sm={12}>
+            <TodoCreateButton />
+          </Col>
+        </Row>
       </Container>
     </React.Fragment>
   )
