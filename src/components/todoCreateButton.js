550be69d12acb8f365009779483db66ecb6c00e8
atamayo <atamayoquintero@gmail.com>
2023-03-17 18:50:13 -0500

    feat: create todoCreateButton and todoSearch component

---
+++ src/components/todoCreateButton.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+
+function TodoCreateButton() {
+  return (
+    <Button className="rounded-pill">
+      <i className="bi bi-plus"></i>
+    </Button>
+  )
+}
+export { TodoCreateButton }
