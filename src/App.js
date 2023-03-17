3b09ca7bac27fb62b7b7c74cdaea3de7d060ef97
atamayo <atamayoquintero@gmail.com>
2023-03-17 18:58:05 -0500

    feat: update todoCounter component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Container, Row, Col } from 'react-bootstrap'
 import { TodoCounter } from './components/todoCounter'
 import { TodoSearch } from './components/todoSearch'
@@ -6,13 +7,22 @@ import { TodoList } from './components/todoList'
 import { TodoItem } from './components/todoItem'
 import { TodoCreateButton } from './components/todoCreateButton'
 
+const api = [
+  { id: 1, text: 'Read for 1 hour', completed: false },
+  { id: 2, text: 'Watering plants', completed: true },
+  { id: 3, text: 'Making the bed', completed: true },
+  { id: 4, text: 'Throw trash', completed: true },
+]
+
 function App() {
+  const [todos, setTodos] = useState(api)
+
   return (
     <React.Fragment>
-      <Container>
-        <Row>
-          <Col sm={12}>
-            <TodoCounter />
+      <Container className="todo__container py-5">
+        <Row className="todo__row m-auto py-3">
+          <Col sm={12} className="d-flex align-items-center">
+            <TodoCounter todo={todos} />
           </Col>
           <Col sm={12}>
             <TodoSearch />
--- src/components/todoCounter.js
+++ src/components/todoCounter.js
@@ -2,20 +2,22 @@
 import { TodoProgressBar } from './todoProgressBar'
 import { Row, Col } from 'react-bootstrap'
 
-function TodoCounter() {
-  const { max, now } = { max: 4, now: 1 }
+function TodoCounter(props) {
+  const { todo } = props
+  const max = todo.length
+  const Now = todo.filter((element) => element.completed).length
 
   return (
-    <Row className="">
+    <Row className="todo__counter w-100 mx-0 mb-2">
       <Col xs={6} className="px-0">
         <h1 className="">App</h1>
         <h1 className="">tasks</h1>
       </Col>
       <Col xs={6} className="d-flex justify-content-end align-items-end px-0">
-        <span>{`${max - now} task left`}</span>
+        <span>{`${max - Now} task left`}</span>
       </Col>
       <Col className="todo__progress-bar px-0">
-        <TodoProgressBar max={max} now={now} />
+        <TodoProgressBar max={max} now={Now} />
       </Col>
     </Row>
   )
