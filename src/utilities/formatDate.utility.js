341bc35c0702411b981255021891fad2b0a9ba72
atamayo <atamayoquintero@gmail.com>
2023-03-20 13:52:43 -0500

    improve: update formatDate service

---
+++ src/utilities/formatDate.utility.js
@@ -0,0 +1,21 @@
+function FormatDate() {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    // year: 'numeric',
+    // hour: 'numeric',
+    // minute: 'numeric',
+    // second: 'numeric',
+    timeZone: timeZone,
+  }
+  const fechaActual = new Date().toLocaleString('en-US', options)
+  return (
+    <div>
+      <p>{fechaActual}</p>
+    </div>
+  )
+}
+
+export { FormatDate }
