d3adf2226fc1f7a7069c90634764b8115cf8bf8b
atamayo <atamayoquintero@gmail.com>
2023-04-09 12:07:59 -0500

    fix: update loadingState

--- src/views/screens/loadingState.js
+++ src/views/screens/loadingState.js
@@ -1,23 +1,29 @@
 import ContentLoader from 'react-content-loader'
+import { Container, Row } from 'react-bootstrap'
 
-export const LoadingState = props => {
+export const LoadingState = (props) => {
   return (
-    <ContentLoader
-      speed={2}
-      width={400}
-      height={160}
-      viewBox="0 0 400 160"
-      backgroundColor="#d8cded"
-      foregroundColor="#c2b8d5"
-      {...props}
-    >
-      <rect x="50" y="6" rx="4" ry="4" width="343" height="38" />
-      <rect x="8" y="6" rx="4" ry="4" width="35" height="38" />
-      <rect x="50" y="55" rx="4" ry="4" width="343" height="38" />
-      <rect x="8" y="55" rx="4" ry="4" width="35" height="38" />
-      <rect x="50" y="104" rx="4" ry="4" width="343" height="38" />
-      <rect x="8" y="104" rx="4" ry="4" width="35" height="38" />
-    </ContentLoader>
+    <Container className="px-0">
+      <Row>
+        <ContentLoader
+          speed={2}
+          width={400}
+          height={200}
+          viewBox="0 0 400 200"
+          backgroundColor="#d8cded"
+          foregroundColor="#c2b8d5"
+          {...props}
+        >
+          <rect x="0" y="6" rx="4" ry="4" width="35" height="38" />
+          <rect x="50" y="6" rx="4" ry="4" width="350" height="38" />
+          <rect x="0" y="55" rx="4" ry="4" width="35" height="38" />
+          <rect x="50" y="55" rx="4" ry="4" width="350" height="38" />
+          <rect x="0" y="104" rx="4" ry="4" width="35" height="38" />
+          <rect x="50" y="104" rx="4" ry="4" width="350" height="38" />
+          <rect x="0" y="153" rx="4" ry="4" width="35" height="38" />
+          <rect x="50" y="153" rx="4" ry="4" width="350" height="38" />
+        </ContentLoader>
+      </Row>
+    </Container>
   )
 }
-
